6bb68fb25f7c91e24c06398b40ca3897f2ee7f6d
Jackson Murphy <jackson@Jacksons-MacBook-Air.local>
2017-03-27 14:23:06 -0600

    cleaned up the AND commands function

--- whoosh.c
+++ whoosh.c
@@ -1,6 +1,6 @@
 /* Written by jackson murphy and the teaching staff of CS4400 at the university of utah.
   This is the main file for the `whoosh` interpreter.
-  Last modified on March 23, 2017
+  Last modified on March 27, 2017
 */
 
 #include <stdlib.h>
@@ -14,13 +14,11 @@ typedef int PIPE[2];
 
 static void run_script(script *scr);
 static void run_group(script_group *group);
-static void run_command(script_group *group);
 static void run_and_commands(script_group *group);
 static void run_or_commands(script_group *group);
 
 static void ctl_c_handler(int sig);
 static void sigchld_handler(int sig);
-static void sigchld_and_handler(int sig);
 static void terminate_processes(pid_t* pids, int num_pids, pid_t except_pid);
 static void set_var(script_var *var, int new_value);
 static void close_unused_pipes(PIPE* pipes_arr, int num_pipes, int open_wr_i, int open_rd_i);
@@ -65,68 +63,13 @@ static void run_group(script_group *group) {
   }
 }
 
-/* This run_command function is for the special case where
-  a group has a single command. */
-static void run_command(script_group *group) {
-  sigset_t sigs, empty_mask;
-  Sigemptyset(&sigs);
-  Sigemptyset(&empty_mask);
-  Signal(SIGINT, ctl_c_handler);
-  Signal(SIGCHLD, sigchld_handler);
-  Sigaddset(&sigs, SIGINT);
-  Sigprocmask(SIG_BLOCK, &sigs, NULL);
-
-  script_command* command = &group->commands[0];
-  pid_t pid;
-  pid_t pids[1];
-  const char **argv = get_argv(&group->commands[0]);
-
-  if((pid = Fork()) == 0){
-    setpgid(0, 0);
-    Execve(argv[0], (char * const *)argv, environ);
-  }
-  pids[0] = pid;
-  free(argv);
-
-  // Wait for signals (Child finished, or received ctl-c, etc.)
-  Sigsuspend(&empty_mask);
-  if(got_ctl_c){
-    terminate_processes(pids, 1, -1);
-    if(group->result_to != NULL)
-      set_var(group->result_to, -2);
-    got_ctl_c = 0;
-  }
-  else{
-    int child_status;
-    Wait(&child_status);
-    if(group->result_to != NULL){
-      if(WIFEXITED(child_status))
-        set_var(group->result_to, WEXITSTATUS(child_status));
-      else
-        set_var(group->result_to, -WTERMSIG(child_status));
-    }
-  }
-}
-
-
 /* This run_and_commands function is run for a group of commands that
   are piped together. */
 static void run_and_commands(script_group *group) {
-  sigset_t sigs, empty_mask;
-  Sigemptyset(&sigs);
-  Sigemptyset(&empty_mask);
-  Signal(SIGINT, ctl_c_handler);
-  Signal(SIGCHLD, sigchld_and_handler);
-  Sigaddset(&sigs, SIGINT);
-  Sigprocmask(SIG_BLOCK, &sigs, NULL);
-
   size_t num_commands = group->num_commands;
-  size_t num_commands_finished = 0;
   size_t num_pipes = num_commands - 1;
   PIPE pipes_arr[(num_commands-1)];
-  pid_t pids[num_commands];
   pid_t pid;
-  int group_not_done = 1;
 
   // Create the pipes
   size_t pipe_i;
@@ -141,7 +84,6 @@ static void run_and_commands(script_group *group) {
     const char **argv = get_argv(&group->commands[0]);
     Execve(argv[0], (char * const *)argv, environ);
   }
-  pids[0] = pid;
   set_pid_var(group, 0, pid);
 
 
@@ -156,7 +98,6 @@ static void run_and_commands(script_group *group) {
       const char **argv = get_argv(&group->commands[command_i]);
       Execve(argv[0], (char * const *)argv, environ);
     }
-    pids[command_i] = pid;
     set_pid_var(group, command_i, pid);
 
   }
@@ -169,40 +110,10 @@ static void run_and_commands(script_group *group) {
     const char **argv = get_argv(&group->commands[num_commands - 1]);
     Execve(argv[0], (char * const *)argv, environ);
   }
-  pids[num_commands - 1] = pid;
   set_pid_var(group, num_commands-1, pid);
-
 
   // Close parent process's pipes
   close_unused_pipes(pipes_arr, num_pipes, -1, -1);
-  //
-  // while(group_not_done){
-  //   Sigsuspend(&empty_mask);
-  //   if(got_ctl_c){
-  //     terminate_processes(pids, num_commands, -1);
-  //     if(group->result_to != NULL)
-  //       set_var(group->result_to, -2);
-  //     group_not_done = 0;
-  //     got_ctl_c = 0;
-  //   }
-  //   else {
-  //     int child_status;
-  //     Wait(&child_status);
-  //
-  //     num_commands_finished++;
-  //     if(num_commands_finished == num_commands){
-  //       group_not_done = 0;
-  //       if(group->result_to != NULL){
-  //         if(WIFEXITED(child_status))
-  //           set_var(group->result_to, WEXITSTATUS(child_status));
-  //         else
-  //           set_var(group->result_to, -WTERMSIG(child_status));
-  //       }
-  //     }
-  //   }
-  // }
-
-
 
   // Wait for each command to finish
   for(command_i = 0; command_i < num_commands; command_i++){
@@ -221,7 +132,8 @@ static void run_and_commands(script_group *group) {
 }
 
 /* This function is run for a group of commands separated
-  by the || operator. All commands are run at the same time,
+  by the || operator, or the special case of a single command.
+  All commands are run at the same time,
   and once any command finishes the rest are terminated. */
 static void run_or_commands(script_group *group) {
   sigset_t sigs, empty_mask;
@@ -278,10 +190,6 @@ static void ctl_c_handler(int sig){
 static void sigchld_handler(int sig){
 }
 
-/*  */
-static void sigchld_and_handler(int sig){
-}
-
 /* Terminate and reap processes in the pids array except for "except_pid"*/
 static void terminate_processes(pid_t* pids, int num_pids, pid_t except_pid){
   int i;
@@ -292,6 +200,7 @@ static void terminate_processes(pid_t* pids, int num_pids, pid_t except_pid){
     }
   }
 }
+
 /* For a commands in a group, sets any pid variables.
   For example, in "/bin/sleep 1000 @ $sleep"
   the variable $sleep is set to the pid of the command*/
@@ -314,6 +223,7 @@ static void close_unused_pipes(PIPE* pipes_arr, int num_pipes, int open_wr_i, in
   }
 }
 
+/* Constructs the argv array for a command */
 const char** get_argv(script_command *command){
   const char **argv;
   int i;
@@ -333,10 +243,8 @@ const char** get_argv(script_command *command){
   return argv;
 }
 
-/* You'll likely want to use this set_var function for converting a
-   numeric value to a string and installing it as a variable's
-   value: */
-
+/* Converts a numeric value to a string and installing it as a variable's
+   value */
 static void set_var(script_var *var, int new_value) {
   char buffer[32];
   free((void *)var->value);
